652d0d8e2e4950e5978e45c64db2183b38340f9b
musik <860577@gmail.com>
2013-04-12 16:57:01 +0800

    remove sphinx

--- lib/recipes/custom.rb
+++ lib/recipes/custom.rb
@@ -4,14 +4,14 @@ Capistrano::Configuration.instance.load do
   namespace :app do
     desc "|Custom| Create database.yml in shared path with settings for current stage and test env"
     task :yml do      
-      #upload './config/resque.yml', "#{shared_path}/config/resque.yml"
+      upload './config/resque.yml', "#{shared_path}/config/resque.yml"
       #upload './config/taobao.yml', "#{shared_path}/config/taobao.yml"
     end 
     task :symlink do
       run "if [ ! -d '#{shared_path}/html' ]; then mkdir #{shared_path}/html; fi;"
       run "rm -rf #{release_path}/public/cache && ln -nfs #{shared_path}/html #{release_path}/public/cache"
       run "rm -rf #{release_path}/config/database.yml && ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-      #run "rm -rf #{release_path}/config/resque.yml && ln -nfs #{shared_path}/config/resque.yml #{release_path}/config/resque.yml"
+      run "rm -rf #{release_path}/config/resque.yml && ln -nfs #{shared_path}/config/resque.yml #{release_path}/config/resque.yml"
       #run "rm -rf #{release_path}/config/taobao.yml && ln -nfs #{shared_path}/config/taobao.yml #{release_path}/config/taobao.yml"
     end
     task :whenever do
