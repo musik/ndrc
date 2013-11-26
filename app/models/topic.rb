0d370dc7d7885c9a1b9f5edc2ff471836b09ac2c
musik <cgicn@qq.com>
2013-11-27 02:17:24 +0800

    others

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -56,9 +56,10 @@ class Topic < ActiveRecord::Base
     def import_from_str str
       str.gsub(/[·． \\” “"。]/,'').gsub(/[，、]/,';').split(";").uniq.compact.each do |s|
         next if s.blank?
-        next if s.match(/^[0-9\.\#\%]+$/).present?
-        next if %(* . \ / ! @ # $ % ^ & ( ) ×).include?(s[-1])
         next if s.count(".") > 1
+        next if %(* . \ / ! @ # $ % ^ & ( ) ×).include?(s[-1])
+        next if s.match(/^[0-9\.\#\%]+$/).present?
+        next if s.match(/^各类|其他|其它/).present?
         #Topic.where(name: s).first_or_create
         Topic.find_or_create_by_name(s)
       end
